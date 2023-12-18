24007d4e352719f3eb15023abf676e238d3bfff2
Paveel2004 <148257086+Paveel2004@users.noreply.github.com>
2023-12-18 05:31:17 +0300

    Добавьте файлы проекта.

---
+++ Data collection/DataCPU.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+namespace Data_collection
+{
+    internal static class DataCPU
+    {
+        public static string GetProcessorArchitecture()
+        { 
+            using (var searcher = new ManagementObjectSearcher("SELECT Architecture FROM Win32_Processor"))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    return obj["Architecture"].ToString();
+                }
+            }
+            return "Неизвестно";
+        }
+        public static string GetProcessorName()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT Name FROM Win32_Processor"))
+            {
+                foreach(ManagementObject obj in searcher.Get())
+                {
+                    return obj["Name"].ToString();
+                }
+            }
+            return "Неизвестно";
+        }
+        public static int GetProcessorCoreCount()
+        {
+            using (var searcher = new ManagementObjectSearcher("SELECT NumberOfCores FROM Win32_Processor"))
+            {
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    return Convert.ToInt32(obj["NumberOfCores"]);
+                }
+                return 0;
+            }
+        }
+        public static double GetProcessorTemperature2()
+        {
+            double cpuTemperature = 0;
+
+            ManagementObjectSearcher mos = new ManagementObjectSearcher(@"root\cimv2", "SELECT * FROM Win32_TemperatureProbe");
+
+            foreach (ManagementObject mo in mos.Get())
+            {
+                double temperature = Convert.ToDouble(mo["CurrentReading"]);
+                cpuTemperature = temperature / 10.0 - 273.15; // Преобразование в градусы Цельсия
+                return cpuTemperature;
+            }
+
+            return 0;
+        }
+
+        public static double GetProcessorTemperature() 
+        {
+            Double CPUtprt = 0;
+            ManagementObjectSearcher mos = new ManagementObjectSearcher(@"root\WMI", "SELECT * FROM MSAcpi_ThermalZoneTemperature");
+            foreach (ManagementObject mo in mos.Get())
+            {
+                CPUtprt = Convert.ToDouble(Convert.ToDouble(mo.GetPropertyValue("CurrentTemperature").ToString()) - 2732) / 10;
+                return CPUtprt;
+            }
+            return 0;
+        }
+    }
+}
---
+++ Data collection/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Management;
+
+namespace Data_collection
+{
+    internal class Program
+    {
+        static void WriteLineRed(string text)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n"+text);
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        static void Main(string[] args)
+        {
+
+            WriteLineRed("Процессор: ");
+
+
+            Console.WriteLine($"Архитектура: {DataCPU.GetProcessorArchitecture()}");
+            Console.WriteLine($"Имя: {DataCPU.GetProcessorName()}");
+            Console.WriteLine($"Количество ядер: {DataCPU.GetProcessorCoreCount()}");
+            Console.WriteLine($"Температура процессора: {DataCPU.GetProcessorTemperature()} *C {DataCPU.GetProcessorTemperature()}");
+
+
+            // Информация об операционной системе
+            WriteLineRed("Операционная система:");
+            Console.WriteLine($"Версия ОС: {Environment.OSVersion}");
+        
+            //Console.WriteLine($"Имя компьютера: {Environment.MachineName}");
+            Console.WriteLine($"Имя пользователя: {Environment.UserName}");
+            //Console.WriteLine($"Версия .NET Framework: {Environment.Version}");
+            //ОС
+            ManagementScope scope = new ManagementScope("\\\\.\\root\\cimv2");
+            ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
+
+            ManagementObjectCollection queryCollection = searcher.Get();
+
+            foreach(ManagementObject m in queryCollection)
+            {
+                Console.WriteLine($"Операционная система: {m["Caption"]}");
+                Console.WriteLine($"Версия: {m["OSArchitecture"]}");
+                Console.WriteLine($"Серийный номер: {m["SerialNumber"]}");
+                Console.WriteLine($"Имя компьютера: {m["CSName"]}");
+                Console.WriteLine($"Количество пользователей: {m["NumberOfUsers"]}");
+                Console.WriteLine($"Состояние: {m["Status"]}");
+
+            }
+
+            // Пути к системным каталогам
+            WriteLineRed("Системные каталоги:");
+            Console.WriteLine($"Каталог системы: {Environment.SystemDirectory}");
+            Console.WriteLine($"Каталог временных файлов: {Environment.GetEnvironmentVariable("TEMP")}");// Информация об операционной системе
+
+
+            //Пользователи 
+            WriteLineRed("Пользователи: ");
+            ObjectQuery userQuery = new ObjectQuery("SELECT * FROM Win32_UserAccount");
+            ManagementObjectSearcher userSearcher = new ManagementObjectSearcher(scope, userQuery);
+
+            ManagementObjectCollection userCollection = userSearcher.Get();
+
+            foreach (ManagementObject user in userCollection)
+            {
+                Console.WriteLine($"Имя пользователя: {user["Name"]}");
+                Console.WriteLine($"Полное имя пользователя: {user["FullName"]}");
+                Console.WriteLine($"SID: {user["SID"]}");
+                Console.WriteLine($"Статус: {user["Status"]}");
+                Console.WriteLine();
+            }
+
+            WriteLineRed("BIOS");
+            ObjectQuery biosQuery = new ObjectQuery("SELECT * FROM Win32_BIOS");
+            ManagementObjectSearcher biosSearcher = new ManagementObjectSearcher(scope, biosQuery);
+
+            ManagementObjectCollection biosCollection = biosSearcher.Get();
+
+            foreach(ManagementObject bios in biosCollection)
+            {
+                Console.WriteLine($"BIOS Version: {bios["Version"]}");
+                Console.WriteLine($"Manufacturer: {bios["Manufacturer"]}");
+                Console.WriteLine($"Description: {bios["Description"]}");
+            }
+
+
+            WriteLineRed("Оперативная память");
+            ObjectQuery memoryQuery = new ObjectQuery("SELECT * FROM Win32_ComputerSystem");
+            ManagementObjectSearcher menorySearcher = new ManagementObjectSearcher(scope, memoryQuery);
+
+            ManagementObjectCollection memoryCollection = menorySearcher.Get();
+
+            foreach(ManagementObject memory in memoryCollection)
+            {
+                ulong totalPhysicalMemory = Convert.ToUInt64(memory["TotalPhysicalMemory"]);
+                Console.WriteLine($"Объем оперативной памяти: {totalPhysicalMemory / (1024 * 1024)} MB");
+            }
+  
+
+        }
+    }
+}
