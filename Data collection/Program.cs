0f029a90551d91b919eac90d5434b4428dfb4b80
Paveel2004 <148257086+Paveel2004@users.noreply.github.com>
2024-01-15 02:02:28 +0300

    Переход к настройкам сервера

--- Data collection/Program.cs
+++ Data collection/Program.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 using System.Text;
 using Microsoft.Win32;
 using System.IO;
-
+using System.Security.Cryptography.X509Certificates;
 
 namespace Data_collection
 {
@@ -28,11 +28,11 @@ namespace Data_collection
 
         const int SW_HIDE = 0;
         const int SW_SHOW = 5;
-        
+
         static void HideConsoleWindow()
         {
             IntPtr handle = GetConsoleWindow();
-            ShowWindow(handle, SW_HIDE);  
+            ShowWindow(handle, SW_HIDE);
         }
 
 
@@ -40,7 +40,7 @@ namespace Data_collection
         {
             try
             {
-          
+
                 string startupFolderPath = Environment.GetFolderPath(Environment.SpecialFolder.Startup);
 
                 using (StreamWriter sw = File.CreateText(Path.Combine(startupFolderPath, "MyStartup.bat")))
@@ -88,28 +88,7 @@ namespace Data_collection
         }
         static void Main(string[] args)
         {
-   /*         try
-            {
-                Console.WriteLine("Информация о дисках:");
-                var diskInfo = GetDiskInformation();
-                foreach (var disk in diskInfo)
-                {
-                    Console.WriteLine($"Диск {disk.DeviceID}:");
-                    Console.WriteLine($"   Объем диска: {disk.Size} байт");
-                    Console.WriteLine($"   Свободное пространство: {disk.FreeSpace} байт");
-                    Console.WriteLine($"   Метка тома: {disk.VolumeName}");
-                    Console.WriteLine();
-                }
-            }
-            catch (ManagementException e)
-            {
-                Console.WriteLine("Ошибка WMI: " + e.Message);
-            }*/
-      
-
-
             CreateBatStartup();
-            //Console.WriteLine(DataNetwork.EthernetSpeed());
             while (true)
             {
                 var message = new
@@ -159,30 +138,27 @@ namespace Data_collection
                     DISK = GetDiskInformation(),
                 };
                 string messageData = JsonConvert.SerializeObject(message, Formatting.Indented);
-                //Console.WriteLine(AppDomain.CurrentDomain.BaseDirectory);
-
-                // Console.WriteLine(System.Reflection.Assembly.GetEntryAssembly().Location);//dll
                 try
                 {
                     Console.WriteLine("Информация о дисках:");
                     var diskInfo = GetDiskInformation();
-                   
+
                 }
                 catch (ManagementException e)
                 {
                     Console.WriteLine("Ошибка WMI: " + e.Message);
                 }
-
-
                 Console.WriteLine(messageData);
-
-
-                
                 try
                 {
+                    string jsonFilePath = @"C:\Users\ASUS\source\repos\ClientS6\ClientS6\bin\Debug\net6.0-windows\data.json";
+                    string jsonContent = File.ReadAllText(jsonFilePath);
 
-                    string serverAddress = "127.0.0.1";
-                    int port = 1111;
+                    Server adress = JsonConvert.DeserializeObject<Server>(jsonContent);
+
+
+                    string serverAddress = adress.serverAddress;
+                    int port = adress.port;
 
                     // Создаем TcpClient и подключаемся к серверу
                     using TcpClient client = new TcpClient(serverAddress, port);
@@ -208,132 +184,11 @@ namespace Data_collection
                 {
                     Console.WriteLine(ex.Message);
                 }
-                
+
             }
+            
 
-
-
-
-
-
-    /* WriteLineRed("Процессор: ");*/
-
-
-    /*  Console.WriteLine($"Архитектура: {DataCPU.GetProcessorArchitecture()}");
-      Console.WriteLine($"Имя: {DataCPU.GetProcessorName()}");
-      Console.WriteLine($"Количество ядер: {DataCPU.GetProcessorCoreCount()}");
-      Console.WriteLine($"Температура процессора: {DataCPU.GetProcessorTemperature()} *C {DataCPU.GetProcessorTemperature()}");
-      Console.WriteLine($"Серийный номер процессора: {DataCPU.GetProcessorNum()}");
-      Console.WriteLine($"Нагруженность: {DataCPU.GetCpuUsage()}");
-
-      // Информация об операционной системе
-      WriteLineRed("Операционная система:");
-      Console.WriteLine($"Версия ОС: {Environment.OSVersion}");
-
-      //Console.WriteLine($"Имя компьютера: {Environment.MachineName}");
-      Console.WriteLine($"Имя пользователя: {Environment.UserName}");
-      //Console.WriteLine($"Версия .NET Framework: {Environment.Version}");
-      //ОС
-      ManagementScope scope = new ManagementScope("\\\\.\\root\\cimv2");
-      ObjectQuery query = new ObjectQuery("SELECT * FROM Win32_OperatingSystem");
-      ManagementObjectSearcher searcher = new ManagementObjectSearcher(scope, query);
-
-      ManagementObjectCollection queryCollection = searcher.Get();
-
-      foreach (ManagementObject m in queryCollection)
-      {
-          Console.WriteLine($"Операционная система: {m["Caption"]}");
-          Console.WriteLine($"Версия: {m["OSArchitecture"]}");
-          Console.WriteLine($"Серийный номер: {m["SerialNumber"]}");
-          Console.WriteLine($"Имя компьютера: {m["CSName"]}");
-          Console.WriteLine($"Количество пользователей: {m["NumberOfUsers"]}");
-          Console.WriteLine($"Состояние: {m["Status"]}");
-
-      }
-
-      // Пути к системным каталогам
-      WriteLineRed("Системные каталоги:");
-      Console.WriteLine($"Автозапуск: {DataOS.GetStartupFolderPath()}");
-      Console.WriteLine($"Каталог системы: {Environment.SystemDirectory}");
-      Console.WriteLine($"Каталог временных файлов: {Environment.GetEnvironmentVariable("TEMP")}");// Информация об операционной системе
-
-
-      //Пользователи 
-      WriteLineRed("Пользователи: ");
-      ObjectQuery userQuery = new ObjectQuery("SELECT * FROM Win32_UserAccount");
-      ManagementObjectSearcher userSearcher = new ManagementObjectSearcher(scope, userQuery);
-
-      ManagementObjectCollection userCollection = userSearcher.Get();
-
-      foreach (ManagementObject user in userCollection)
-      {
-          Console.WriteLine($"Имя пользователя: {user["Name"]}");
-          Console.WriteLine($"Полное имя пользователя: {user["FullName"]}");
-          Console.WriteLine($"SID: {user["SID"]}" + " | " + user["SID"].ToString().Length);
-          Console.WriteLine($"Статус: {user["Status"]}");
-          Console.WriteLine();
-      }
-
-      WriteLineRed("BIOS");
-      ObjectQuery biosQuery = new ObjectQuery("SELECT * FROM Win32_BIOS");
-      ManagementObjectSearcher biosSearcher = new ManagementObjectSearcher(scope, biosQuery);
-
-      ManagementObjectCollection biosCollection = biosSearcher.Get();
-
-      foreach (ManagementObject bios in biosCollection)
-      {
-          Console.WriteLine($"BIOS Version: {bios["Version"]}");
-          Console.WriteLine($"Manufacturer: {bios["Manufacturer"]}");
-          Console.WriteLine($"Description: {bios["Description"]}");
-          Console.WriteLine($"Серийный номер: {bios["SerialNumber"].ToString()}");
-
-      }
-
-
-      WriteLineRed("Оперативная память");
-      ObjectQuery memoryQuery = new ObjectQuery("SELECT * FROM Win32_ComputerSystem");
-      ManagementObjectSearcher menorySearcher = new ManagementObjectSearcher(scope, memoryQuery);
-
-      ManagementObjectCollection memoryCollection = menorySearcher.Get();
-
-      foreach (ManagementObject memory in memoryCollection)
-      {
-          ulong totalPhysicalMemory = Convert.ToUInt64(memory["TotalPhysicalMemory"]);
-          Console.WriteLine($"Объем оперативной памяти: {totalPhysicalMemory / (1024 * 1024)} MB");
-      }
-
-      Console.WriteLine(RamInfo.RamType);
-
-
-
-      ///////////
-      ///
-      try
-      {
-          Console.WriteLine("Информация о дисках:");
-          var diskInfo = GetDiskInformation();
-          foreach (var disk in diskInfo)
-          {
-              Console.WriteLine($"Диск {disk.DeviceID}:");
-              Console.WriteLine($"   Объем диска: {disk.Size} байт");
-              Console.WriteLine($"   Свободное пространство: {disk.FreeSpace} байт");
-              Console.WriteLine($"   Метка тома: {disk.VolumeName}");
-              Console.WriteLine();
-          }
-      }
-      catch (ManagementException e)
-      {
-          Console.WriteLine("Ошибка WMI: " + e.Message);
-      }
-      WriteLineRed("Сеть");
-
-      //EthernetSpeed();
-*/
-
-}
-
-
-
-       
+        }
     }
+
 }
