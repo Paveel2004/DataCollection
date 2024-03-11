5d23a50a60e2dade664e976de60bb6f63547be4d
Paveel2004 <148257086+Paveel2004@users.noreply.github.com>
2024-03-11 09:24:40 +0300

    Добавлен класс ServerMessageSender

--- Data collection/Connection/ServerMessageSender.cs
+++ Data collection/Connection/ServerMessageSender.cs
@@ -1,13 +1,101 @@
-﻿using System;
+﻿using GlobalClass.Dynamic_data;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data_collection.Connection
 {
-    internal class ServerMessageSender
+    internal static class ServerMessageSender
     {
-        
+        public static void SendMessageUsage<T>(string serverAddress, int port, string message)
+        {
+            T obj = JsonConvert.DeserializeObject<T>(message);
+            var type = typeof(T);
+            PropertyInfo property;
+
+            if (obj is UsageRAM)
+            {
+                property = type.GetProperty("Workload");
+                string currentWorkload = property.GetValue(obj)?.ToString();
+                if (lastUsageRam != currentWorkload)
+                {
+                    SendMessage(serverAddress, port, message);
+                    lastUsageRam = currentWorkload;
+                }
+            }
+            if (obj is UsageOS)
+            {
+                property = type.GetProperty("Status");
+                string currentStatus = property.GetValue(obj)?.ToString();
+                if (lastUsageOS != currentStatus)
+                {
+                    SendMessage(serverAddress, port, message);
+                    lastUsageOS = currentStatus;
+                }
+            }
+            if (obj is UsageCPU)
+            {
+                var property2 = type.GetProperty("Temperature");
+                property = type.GetProperty("Workload");
+
+                string currentTemperature = property2.GetValue(obj)?.ToString();
+
+                string currentWorkload = property.GetValue(obj)?.ToString();
+
+                string currentStatus = currentWorkload + '|' + currentTemperature;
+                if (lastUsageCPU != currentStatus)
+                {
+                    SendMessage(serverAddress, port, message);
+                    lastUsageCPU = currentStatus;
+                }
+            }
+            if (obj is UsageEthernet)
+            {
+                property = type.GetProperty("Speed");
+                string cerrentSpeed = property.GetValue(obj)?.ToString();
+                if (lastEthernetSpeed != cerrentSpeed)
+                {
+                    SendMessage(serverAddress, port, message);
+                    lastEthernetSpeed = cerrentSpeed;
+                }
+            }
+
+        }
+        private static string lastEthernetSpeed = null;
+        private static string lastUsageRam = null;
+        private static string lastUsageOS = null;
+        private static string lastUsageCPU = null;
+        public static void SendMessage(string serverAddress, int port, string message)
+        {
+            try
+            {
+                // Создаем TcpClient и подключаемся к серверу
+                using TcpClient client = new TcpClient(serverAddress, port);
+                Console.WriteLine($"Подключено к серверу на порту {port}...");
+
+                // Получаем поток для обмена данными с сервером
+                using NetworkStream stream = client.GetStream();
+
+                // Отправляем сообщение серверу
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+                Console.WriteLine($"Отправлено сообщение: {message}");
+
+                // Читаем ответ от сервера
+                data = new byte[256];
+                int bytesRead = stream.Read(data, 0, data.Length);
+                string response = Encoding.UTF8.GetString(data, 0, bytesRead);
+                Console.WriteLine($"Ответ от сервера: {response}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
--- Data collection/Program.cs
+++ Data collection/Program.cs
@@ -16,6 +16,7 @@ using System.Runtime.CompilerServices;
 using System.CodeDom;
 using System.Reflection;
 using Newtonsoft.Json.Linq;
+using Data_collection.Connection;
 
 namespace Data_collection
 {
@@ -94,90 +95,8 @@ namespace Data_collection
             return diskInfo;
         }
 
-        static void SendMessage(string serverAddress, int port, string message)
-        {
-            try
-            {
-                // Создаем TcpClient и подключаемся к серверу
-                using TcpClient client = new TcpClient(serverAddress, port);
-                Console.WriteLine($"Подключено к серверу на порту {port}...");
 
-                // Получаем поток для обмена данными с сервером
-                using NetworkStream stream = client.GetStream();
-
-                // Отправляем сообщение серверу
-                byte[] data = Encoding.UTF8.GetBytes(message);
-                stream.Write(data, 0, data.Length);
-                Console.WriteLine($"Отправлено сообщение: {message}");
-
-                // Читаем ответ от сервера
-                data = new byte[256];
-                int bytesRead = stream.Read(data, 0, data.Length);
-                string response = Encoding.UTF8.GetString(data, 0, bytesRead);
-                Console.WriteLine($"Ответ от сервера: {response}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-        static void SendMessageUsage<T>(string serverAddress, int port, string message)
-        {
-            T obj = JsonConvert.DeserializeObject<T>(message);
-            var type = typeof(T);
-            PropertyInfo property;
-
-            if (obj is UsageRAM) {
-                property = type.GetProperty("Workload");
-                string currentWorkload = property.GetValue(obj)?.ToString();
-                if (lastUsageRam != currentWorkload)
-                {
-                    SendMessage(serverAddress, port, message);
-                    lastUsageRam = currentWorkload;
-                }
-            }
-            if (obj is UsageOS) 
-            {
-                property = type.GetProperty("Status");
-                string currentStatus = property.GetValue(obj)?.ToString();
-                if(lastUsageOS != currentStatus)
-                {
-                    SendMessage(serverAddress, port, message);
-                    lastUsageOS = currentStatus;
-                }
-            }
-            if (obj is UsageCPU)
-            {
-                var property2 = type.GetProperty("Temperature");
-                property = type.GetProperty("Workload");
-
-                string currentTemperature = property2.GetValue(obj)?.ToString();
-
-                string currentWorkload = property.GetValue(obj)?.ToString();
-
-                string currentStatus = currentWorkload + '|' + currentTemperature;
-                if (lastUsageCPU != currentStatus)
-                {
-                    SendMessage(serverAddress, port, message);
-                    lastUsageCPU = currentStatus;
-                }
-            }
-            if(obj is UsageEthernet) 
-            {
-                property = type.GetProperty("Speed");
-                string cerrentSpeed = property.GetValue(obj)?.ToString();
-                if (lastEthernetSpeed != cerrentSpeed)
-                {
-                    SendMessage(serverAddress, port, message);
-                    lastEthernetSpeed = cerrentSpeed;
-                }
-            }
-
-        }
-        private static string lastEthernetSpeed = null;
-        private static string lastUsageRam = null;
-        private static string lastUsageOS = null;
-        private static string lastUsageCPU = null;
+        
         static void Main(string[] args)
         {
             //HideConsoleWindow();
@@ -185,39 +104,25 @@ namespace Data_collection
           
                 try
                 {
-
                     string jsonFilePath = @"C:\Users\ASUS\source\repos\ClientS6\ClientS6\bin\Debug\net6.0-windows\data.json";
                     string jsonContent = File.ReadAllText(jsonFilePath);
                     Server adress = JsonConvert.DeserializeObject<Server>(jsonContent);
-                    DeviceData<NetworkInterfaceData> networkData = new()
-                    {
-                        Data = NetworkInformationGatherer.GetNetworkInterfaces(),
-                        SerialNumberBIOS = InformationGathererBIOS.GetBiosSerialNumber()
-                    };
-
-                    DeviceData<CPUData> DataCPU = new()
-                    {
-                        SerialNumberBIOS = InformationGathererBIOS.GetBiosSerialNumber(),
-                        Data = InformationGathererCPU.GetCPU()
-                    };
-                    DeviceData<RAMData> DataRAM = new()
-                    {
-                        SerialNumberBIOS = InformationGathererBIOS.GetBiosSerialNumber(),
-                        Data = InformationGathererRAM.GetRAM()
+                    DeviceData<NetworkInterfaceData> DataNetwork = new(){Data = NetworkInformationGatherer.GetNetworkInterfaces(),SerialNumberBIOS = InformationGathererBIOS.GetBiosSerialNumber()};
+                    DeviceData<CPUData> DataCPU = new(){SerialNumberBIOS = InformationGathererBIOS.GetBiosSerialNumber(),Data = InformationGathererCPU.GetCPU()};
+                    DeviceData<RAMData> DataRAM = new(){ SerialNumberBIOS = InformationGathererBIOS.GetBiosSerialNumber(), Data = InformationGathererRAM.GetRAM()
                     };
                     string serverAddress = adress.serverAddress;
 
-                    SendMessage(serverAddress, 9930, JsonHelper.SerializeDeviceData(networkData));
-                    SendMessage(serverAddress, 9860, JsonHelper.SerializeDeviceData(DataCPU));
-                    SendMessage(serverAddress, 9790, JsonHelper.SerializeDeviceData(DataRAM));
+                    ServerMessageSender.SendMessage(serverAddress, 9930, JsonHelper.SerializeDeviceData(DataNetwork));
+                    ServerMessageSender.SendMessage(serverAddress, 9860, JsonHelper.SerializeDeviceData(DataCPU));
+                    ServerMessageSender.SendMessage(serverAddress, 9790, JsonHelper.SerializeDeviceData(DataRAM));
 
                     while (true)
                     {
-                        SendMessageUsage<UsageRAM>(serverAddress, 9720, JsonConvert.SerializeObject(new UsageRAM(InformationGathererRAM.GetUsageRam(),InformationGathererBIOS.GetBiosSerialNumber())));
-                        SendMessageUsage<UsageOS>(serverAddress, 9650, JsonConvert.SerializeObject(new UsageOS(InformationGathererUser.GetUserName(), OSInformationGatherer.GetSystemState(), InformationGathererBIOS.GetBiosSerialNumber())));
-                        SendMessageUsage<UsageCPU>(serverAddress, 9580, JsonConvert.SerializeObject(new UsageCPU(InformationGathererCPU.GetProcessorTemperature(), InformationGathererCPU.GetCpuUsage(), InformationGathererBIOS.GetBiosSerialNumber())));
-                        SendMessageUsage<UsageEthernet>(serverAddress, 9510, JsonConvert.SerializeObject(new UsageEthernet(NetworkInformationGatherer.EthernetSpeed(), InformationGathererBIOS.GetBiosSerialNumber())));
-                        
+                        ServerMessageSender.SendMessageUsage<UsageRAM>(serverAddress, 9720, JsonConvert.SerializeObject(new UsageRAM(InformationGathererRAM.GetUsageRam(),InformationGathererBIOS.GetBiosSerialNumber())));
+                        ServerMessageSender.SendMessageUsage<UsageOS>(serverAddress, 9650, JsonConvert.SerializeObject(new UsageOS(InformationGathererUser.GetUserName(), OSInformationGatherer.GetSystemState(), InformationGathererBIOS.GetBiosSerialNumber())));
+                        ServerMessageSender.SendMessageUsage<UsageCPU>(serverAddress, 9580, JsonConvert.SerializeObject(new UsageCPU(InformationGathererCPU.GetProcessorTemperature(), InformationGathererCPU.GetCpuUsage(), InformationGathererBIOS.GetBiosSerialNumber())));
+                        ServerMessageSender.SendMessageUsage<UsageEthernet>(serverAddress, 9510, JsonConvert.SerializeObject(new UsageEthernet(NetworkInformationGatherer.EthernetSpeed(), InformationGathererBIOS.GetBiosSerialNumber())));  
                     }            
                 }
                 catch (Exception ex)
