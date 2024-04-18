54562804f38dc73c1be31e41493cbd76d3b3d825
Paveel2004 <148257086+Paveel2004@users.noreply.github.com>
2024-04-18 06:17:27 +0300

    Добавлен класс WebControl

---
+++ Data collection/Control/WebControl.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_collection.Control
+{
+    internal static class WebControl
+    {
+        private static string path = @"C:\Windows\System32\drivers\etc\hosts";
+        public static void BlockedWebsite(StringBuilder address)
+        {
+            StringBuilder record = new("127.0.0.1 ");
+            record.Append(address.ToString());
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(record);
+            }
+        }
+        public static void UnBlockedWebSite(string address)
+        {
+            var lines = File.ReadAllLines(path).ToList();
+            lines.RemoveAll(line => line.Contains(address));
+            File.WriteAllLines(path, lines);
+        }
+    }
+}
