4ab2d717e09483d5b5556a5edcdbf11c56ca3c62
Paveel2004 <148257086+Paveel2004@users.noreply.github.com>
2024-05-26 04:48:30 +0300

    Добавлен клас PowerShell

---
+++ Data collection/PowerShell.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_collection
+{
+    internal static class PowerShell
+    {
+        public static List<string> GetPowershellValueList(string table, string column)
+        {
+            // Команда PowerShell
+            string powerShellCommand = $"Get-{table} | Format-Table {column}";
+
+            // Выполняем команду PowerShell и получаем результат
+            string powerShellOutput = RunPowerShellCommand(powerShellCommand);
+
+            // Разделяем результат на строки по разделителю новой строки
+            string[] lines = powerShellOutput.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+            // Пропускаем первые две строки, содержащие разделители и заголовки
+            List<string> serialNumbers = new List<string>();
+            for (int i = 2; i < lines.Length; i++)
+            {
+                serialNumbers.Add(lines[i]);
+            }
+            // Возвращаем список строк с серийными номерами
+            return serialNumbers;
+        }
+        static string RunPowerShellCommand(string command)
+        {
+            // Создаем новый процесс для выполнения команды PowerShell
+            Process process = new Process();
+            process.StartInfo.FileName = "powershell.exe";
+            process.StartInfo.Arguments = command;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.Start();
+
+            // Считываем результат выполнения команды
+            string output = process.StandardOutput.ReadToEnd();
+
+            // Ждем, пока процесс завершится
+            process.WaitForExit();
+
+            // Возвращаем результат
+            return output;
+        }
+    }
+}
